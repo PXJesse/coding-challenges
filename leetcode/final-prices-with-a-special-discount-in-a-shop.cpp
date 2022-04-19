5d159fdc16de7ebe00427021738b85a64b82f43e
Jesse <44534584+PXJesse@users.noreply.github.com>
2022-04-19 23:08:54 +0200

    LeetCode Challenge 1475
    Final Prices With a Special Discount in a Shop

---
+++ leetcode/final-prices-with-a-special-discount-in-a-shop.cpp
@@ -0,0 +1,62 @@
+#include <vector>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+vector<int> finalPrices(vector<int>& input);
+void printVector(vector<int> input);
+
+
+int main() {
+    vector<int> input = { 10, 1, 1, 6 };
+    printVector(finalPrices(input));
+
+    return 0;
+}
+
+
+/**
+ * Calculate and return the prices from an array of input prices
+ * given a special discount.
+ * 
+ * The discount is calculated from the ith element onwards. The
+ * discount is equivalent to the value of the first item after i
+ * that is cheaper or of the same price.
+ * 
+ * @param input vector int array of prices
+ * @returns vector int array of modified prices
+ */
+vector<int> finalPrices(vector<int>& input) {
+    int len = input.size();
+
+    // For each price, loop through the remainder of the array
+    for (int i = 0; i < len; i++) {
+        for (int j = i + 1; j < len; j++) {
+
+            // As soon as the price of a future index j <= the price of i, apply
+            // the discount (subtract price of j) and move on to the next price
+            if (input[j] <= input[i]) {
+                input[i] -= input[j];
+                break;
+            }
+        }
+    }
+    
+    return input;
+};
+
+
+/**
+ * Utility: Print a given vector v
+ * 
+ * @param v vector
+ * @returns none
+ */
+void printVector(vector<int> v) {
+    cout << "Output: ";
+    for (auto& i : v) {
+        cout << i << " ";
+    }
+    cout << "\n";
+};
---
+++ readme.md
@@ -0,0 +1,8 @@
+# ⚙️ Coding Challenges
+
+My collection of code challenges worked out in C++, a language I've been wanting to learn for quite some time.
+
+## Leetcode problems
+|  #  | Problem         |  Solution       |  Difficulty     |
+|-----|---------------- | --------------- | --------------- |
+1475 | [Final Prices With a Special Discount in a Shop](https://leetcode.com/problems/final-prices-with-a-special-discount-in-a-shop/) | [C++](./leetcode/final-prices-with-a-special-discount-in-a-shop.cpp) | Easy
